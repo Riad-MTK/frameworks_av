09b6937efb9f44de571660cf12d12698a4aa57f0
Vanir Aosp-mtk <riadkhan2367@gmail.com>
2016-09-16 07:54:06 +0600

    Added_MTK_Camera_support From Ferhung-mtk

--- camera/Android.mk
+++ camera/Android.mk
@@ -36,7 +36,12 @@ LOCAL_SRC_FILES:= \
 	camera2/OutputConfiguration.cpp \
 	CameraBase.cpp \
 	CameraUtils.cpp \
-	VendorTagDescriptor.cpp
+	VendorTagDescriptor.cpp \
+	
+	ifeq ($(BOARD_HAS_MTK_HARDWARE),true)
+	LOCAL_SRC_FILES += \
+		MtkCameraParameters.cpp
+endif
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
